c5cfd68483f7b11a4083ef500f5aa360e4b923f5
iMac <imac@iMacs-iMac.local>
2022-06-15 12:39:43 +0100

    style: enhance breakpoint at index page

--- components/FeaturedProduct.vue
+++ components/FeaturedProduct.vue
@@ -5,7 +5,6 @@
     </div>
 
     <div class="featured-product__info flex justify-between py-3">
-      <!-- <NuxtLink :to="featuredProductRoute" class="featured-product__info_name text-normal text-left w-3/5">{{ product.title.slice(0, 18) }}...</NuxtLink> -->
       <NuxtLink :to="`/FeaturedProduct/${product.id}`" class="featured-product__info_name text-normal text-left w-3/5">{{ product.title.slice(0, 18) }}...</NuxtLink>
       <div class="featured-product__info_price text-normal font-bold">$ {{ product.price }}</div>
     </div>
--- components/Featuredproducts.vue
+++ components/Featuredproducts.vue
@@ -1,13 +1,12 @@
 <template>
   <section class="featured-products mb-6 p-6">
-    <h1 class="text-normal text-3xl font-bold pb-9">Featured Products</h1>
+    <h1 class="text-normal text-3xl font-bold text-center pb-9">Featured Products</h1>
 
-    <div class="featured-products-grid-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mb-6">
+    <div class="featured-products-grid-container grid grid-cols-1 sm:grid-cols-2 mlg:grid-cols-4 gap-6 mb-6">
       <FeaturedProduct v-for="product in featuredProducts" :key="product.id" :product="product" />
     </div>
 
     <div class="view-all text-center">
-      <!-- <NuxtLink :to="featuredProductsRoute" class="text-normal text-xl font-bold"> -->
       <NuxtLink to="/FeaturedProducts" class="text-normal text-xl font-bold">
           <span class="material-icons">arrow_right_alt</span>
         <br />
@@ -22,10 +21,6 @@ import { useStore } from '~/stores/store';
 import { computed } from 'vue';
 
 const store = useStore();
-
-const featuredProductsRoute = computed(() => ({
-  name: 'FeaturedProducts',
-}))
 
 const featuredProducts = computed(() => store.getProducts.slice(0, 4));
 </script>
