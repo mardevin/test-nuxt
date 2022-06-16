8ee031ff05f1474ac699f97d6ed27cb2ece6df37
mardevin <marouane@devinweb.com>
2022-06-16 11:16:30 +0100

    feat: show red dot if there are items in cart

--- layouts/Navbar/Navbar.vue
+++ layouts/Navbar/Navbar.vue
@@ -1,8 +1,5 @@
 <template>
   <header class="p-6">
-    <!-- <div class="search text-right">
-      <input type="search" name="search" id="search" class="hidden mlg:inline-block mlg:mb-2 border-2 border-blue-400" />
-    </div> -->
     <div class="header-nav flex justify-between items-center">
       <div class="logo text-weak text-2xl md:text-5xl flex items-center">
           <span class="material-icons text-5xl mr-2">sports_baseball</span>
@@ -19,9 +16,13 @@
             <span class="material-icons">home</span>
           </li>
           <li class="inline mx-3">CATEGORIES</li>
-          <li class="inline mx-3">
-            <span class="material-icons">shopping_cart</span>
+          <li class="inline mx-3 relative">
+            <span class="material-icons">
+              shopping_cart
+            </span>
+            <div v-if="!isCartEmpty" class="bg-red-500 absolute right-0.5 bottom-0.5 w-2 h-2 rounded-full"></div>
           </li>
+          <li v-if="!isCartEmpty" class="text-strong font-bold bg-weak text-center inline mx-3">{{ cartItems }} items</li>
         </ul>
       </nav>
       <input type="search" name="search" id="search" class="hidden mlg:inline-block mlg:mb-2 border-2 border-blue-400" />
@@ -37,17 +38,28 @@
         <span class="material-icons">home</span>
       </li>
       <li>CATEGORIES</li>
-      <li>
-        <span class="material-icons">shopping_cart</span>
+      <li class="text-center">
+        <div class="inline-block relative">
+          <span class="material-icons block">
+            shopping_cart
+          </span>
+          <div v-if="!isCartEmpty" class="bg-red-500 absolute right-0.5 bottom-0.5 w-2 h-2 rounded-full"></div>
+        </div>
       </li>
+      <li v-if="!isCartEmpty" class="text-strong font-bold bg-weak text-center inline mx-3">{{ cartItems }} items</li>
     </ul>
   </nav>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { useStore } from '~/stores/store';
+import { ref, computed } from 'vue';
+
+const store = useStore();
 
 const showDropdownMenu = ref(false);
+const cartItems = computed(() => store.getNumberOfCartItems);
+const isCartEmpty = computed(() => cartItems.value === 0);
 
 function toggleShowDropdownMenu() {
   showDropdownMenu.value = !showDropdownMenu.value;
