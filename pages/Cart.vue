d38a01313ca037761b3fb12c247edb3614d581e7
mardevin <marouane@devinweb.com>
2022-06-17 12:21:10 +0100

    feat: display cart items in cart page

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -1,11 +1,14 @@
 <template>
-  <main>
-    <div v-if="isCartEmpty">
-      Your cart is currently empty. <NuxtLink to="FeaturedProducts">Go add some products</NuxtLink>.
+  <main class="p-6">
+    <h1 class="text-2xl font-bold mb-6">Welcome to your cart</h1>
+    <div v-if="isCartEmpty" class="text-md md:text-4xl flex justify-center items-center h-30 md:h-60 my-16">
+      Your cart is currently empty. &nbsp; <NuxtLink to="FeaturedProducts" class="text-blue-600">Go add some products</NuxtLink>.
     </div>
 
     <div v-else>
-      YEEEEEE
+      <div class="grid grid-cols-1 sm:grid-cols-2 mlg:grid-cols-4 gap-6 mb-6">
+        <FeaturedProduct v-for="cartItem in cart" :key="cartItem.product.id" :product="cartItem.product" />
+      </div>
     </div>
   </main>
 </template>
@@ -16,6 +19,7 @@ import { computed } from 'vue';
 
 const store = useStore();
 
+const cart = computed(() => store.getCart);
 const cartItems = computed(() => store.getNumberOfCartItems);
 const isCartEmpty = computed(() => cartItems.value === 0);
 
