f2326a862decd3f150834ec93b64e6252e97b76c
mardevin <marouane@devinweb.com>
2022-06-17 17:00:28 +0100

    refactor: remove redundant code

--- pages/FeaturedProduct/[productId].vue
+++ pages/FeaturedProduct/[productId].vue
@@ -1,8 +1,5 @@
 <template>
   <main>
-    <div class="nav-tree text-normal text-xs text-left mb-1 p-6">
-      Home > Category Name > Product Name
-    </div>
     <ProductInfo />
     <hr class="mb-12" />
     <SuggestedProducts />
