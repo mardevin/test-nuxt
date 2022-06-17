47a04b26064974bb95fb125c14d2c621f3ff3291
mardevin <marouane@devinweb.com>
2022-06-17 12:41:40 +0100

    feat: hero slide

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,14 +2,16 @@
 
   <!-- Carousel work-in-progress -->
   <section class="carousel-container relative max-w-full mx-auto mb-12 p-6">
-    <div class="carousel bg-weak flex flex-col md:flex-row md:justify-between">
-      <div class="picture hidden md:block md:w-1/2"></div>
-      <div class="description md:text-right py-10 px-6">
-        <h1 class="text-normal text-3xl sm:text-6xl font-bold mb-8">Suspendisse interdum rutrum nise.</h1>
-        <p class="text-normal text-2xl">Ut tellus quam, venenatis non tempus vitae, volupat blandit libero eget elos</p>
+    <div v-for="slide in slides" :key="slide" class="carousel">
+      <div v-show="slide === activeSlide" class="bg-weak flex flex-col md:flex-row md:justify-between">
+        <div class="picture hidden md:block md:w-1/2"></div>
+        <div class="description md:text-right py-10 px-6">
+          <h1 class="text-normal text-3xl sm:text-6xl font-bold mb-8">{{ slide }} Suspendisse interdum rutrum nise.</h1>
+          <p class="text-normal text-2xl">Ut tellus quam, venenatis non tempus vitae, volupat blandit libero eget elos</p>
+        </div>
       </div>
     </div>
-    <div class="carousel hidden bg-weak flex flex-col md:flex-row md:justify-between">
+    <!-- <div class="carousel hidden bg-weak flex flex-col md:flex-row md:justify-between">
       <div class="picture md:w-1/2 bg-green-300 p-3"></div>
       <div class="description md:text-right p-3">
         <h1 class="text-normal text-2xl font-bold">Suspendisse interdum rutrum nise.</h1>
@@ -22,12 +24,15 @@
         <h1 class="text-normal text-2xl font-bold">Suspendisse interdum rutrum nise.</h1>
         <p class="text-normal">Ut tellus quam, venenatis non tempus vitae, volupat blandit libero eget elos</p>
       </div>
-    </div>
+    </div> -->
 
     <div class="dots text-center">
-      <span class="dot bg-gray-400 inline-block w-2 h-2 my-0 mx-2 rounded-full cursor-pointer"></span>
-      <span class="dot bg-gray-400 inline-block w-2 h-2 my-0 mx-2 rounded-full cursor-pointer"></span>
-      <span class="dot bg-gray-400 inline-block w-2 h-2 my-0 mx-2 rounded-full cursor-pointer"></span>
+      <span 
+        v-for="slide in slides" 
+        :key="slide" 
+        class="dot bg-gray-400 inline-block w-2 h-2 my-0 mx-2 rounded-full cursor-pointer"
+        @click="activeSlide = slide"
+      ></span>
     </div>
   </section>
 </template>
@@ -35,5 +40,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const activeSlide = ref(0);
+const activeSlide = ref(1);
+const slides = ref([1, 2, 3]);
 </script>
