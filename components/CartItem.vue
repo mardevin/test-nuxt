15e5fab0ce57f50c02e32b7571e6da9fd79a15fd
marouane@devinweb.com <imac@iMacs-iMac.local>
2022-06-20 12:06:22 +0100

    style: redesign cart items

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="cart-item">
-    <div class="cart-item-container">
+  <div class="cart-item bg-gray-100 mb-6 p-6 rounded-lg">
+    <div class="cart-item-container mb-3">
       <div class="cart-item-info flex">
-        <div class="cart-item-image">
-          <img :src="product.thumbnail" :alt="product.title" />
+        <div class="cart-item-image w-16 h-16 mr-3">
+          <img :src="product.thumbnail" :alt="product.title" class="w-full h-full" />
         </div>
-        <div class="cart-item main md:flex md:justify-between">
+        <div class="cart-item main md:flex md:justify-between w-full">
           <div class="cart-item-general">
-            <h1 class="cart-item-title">
+            <h1 class="cart-item-title mb-1">
               {{ product.title }}
             </h1>
-            <div class="cart-item-brand">
-              {{ product.brand }}
+            <div class="cart-item-brand text-sm">
+              <span class="text-gray-400">Brand: </span> {{ product.brand }}
             </div>
           </div>
-        </div>
 
-        <div class="cart-item-price">
-          <h2>$ {{ product.price }}</h2>
+          <div class="cart-item-price text-xl font-bold">
+            <h2>$ {{ product.price }}</h2>
+          </div>
         </div>
       </div>
     </div>
     
-    <div class="cart-item-call-to-action">
+    <div class="cart-item-call-to-action flex justify-between items-center">
       <div class="remove">
-        <button type="button" @click="removeProductFromCart(product)">
-          <span class="material-icons">remove</span>
+        <button type="button" class="text-red-400" @click="removeProductFromCart(product)">
+          <span class="material-icons text-3xl">delete</span>
         </button>
       </div>
       <div class="buttons ml-3">
-        <button class="text-white bg-normal font-light py-2 px-3 rounded-full" @click="addToCart(product)">
+        <span class="number-of-items text-3xl">{{ numberOfItems }}</span>
+        <button class="text-white bg-normal font-light ml-3 py-2 px-3 rounded-full" @click="addToCart(product)">
           <span class="material-icons">add</span>
         </button>
         <button class="text-normal font-light ml-3 py-2 px-3 rounded-full border border-3 border-normal" @click="removeFromCart(product)">
